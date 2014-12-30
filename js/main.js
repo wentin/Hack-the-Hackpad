aeb2307fc10fd170ae3aea83120aa1ba42947416
wentin <zhangwenting111@gmail.com>
2014-12-30 07:06:51 -0500

    init commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    var hash = window.location.hash?window.location.hash:'dK2se3bCFHo';
+    hash = hash.replace("#", "");
+    var load_url = 'https://hackpad.com/ep/pad/static/' + hash;
+
+    $.ajax({
+        url: load_url,
+        type: 'GET',
+        success: function(res) {
+            var text = res.responseText;
+            // then you can manipulate your text as you wish
+            console.log(text);
+            text = text.replace("<p><strong>", "<p class='heading3'><strong>");
+            $('article').html(text);
+        }
+    }); 
+})
